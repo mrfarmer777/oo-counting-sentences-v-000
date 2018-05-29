56cebc8218eed70fd850794be5accfb32ad4b90e
Matt Farmer <mrfarmer777@gmail.com>
2018-05-29 19:39:04 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    #why not count the groups of punctiation as a means of finding the sentence/question/exclamation endings?
+    res = self.scan(/[!.?]+/) #find any number of these guys in a row and return them to an array.
+    return res.length
   end
 end
